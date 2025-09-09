09776353720f8a882c36be6296428cc424491f7e
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-09-09 09:34:24 -0500

    Changed landing page to home screen

--- app/src/main/java/silas/dev/neatly/MainActivity.kt
+++ app/src/main/java/silas/dev/neatly/MainActivity.kt
@@ -6,11 +6,16 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.hilt.navigation.compose.hiltViewModel
+import silas.dev.neatly.ui.home.HomeScreen
+import silas.dev.neatly.ui.home.HomeScreenViewModel
 import silas.dev.neatly.ui.theme.NeatlyTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,17 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NeatlyTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
+                val viewModel: HomeScreenViewModel = hiltViewModel()
+
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    HomeScreen(
+                        viewModel = viewModel
                     )
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NeatlyTheme {
-        Greeting("Android")
     }
 }
