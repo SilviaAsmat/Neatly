ceef892e3a90d2d1a799e9720ff4bb4009f7e3db
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-09-10 12:56:42 -0500

    Added method to get all collections with products

--- app/src/main/java/silas/dev/neatly/data/RepositoryImpl.kt
+++ app/src/main/java/silas/dev/neatly/data/RepositoryImpl.kt
@@ -4,6 +4,7 @@ import silas.dev.neatly.data.room.NeatlyDatabase
 import silas.dev.neatly.data.room.collection.CollectionsEntity
 import silas.dev.neatly.data.room.product_info.ProductInfoEntity
 import silas.dev.neatly.domain.CollectionInfo
+import silas.dev.neatly.domain.CollectionWithProducts
 import silas.dev.neatly.domain.ProductInfo
 import silas.dev.neatly.domain.Repository
 import javax.inject.Inject
@@ -11,7 +12,7 @@ import javax.inject.Inject
 class RepositoryImpl @Inject constructor(
     private val database: NeatlyDatabase
 ) : Repository {
-    override suspend fun addProduct(product: ProductInfo){
+    override suspend fun addProduct(product: ProductInfo) {
         val productEntity = ProductInfoEntity(
             productId = 0,
             name = product.name,
@@ -21,7 +22,7 @@ class RepositoryImpl @Inject constructor(
         database.productInfoDao().insertProduct(productEntity)
     }
 
-    override suspend fun addCollection(collection: CollectionInfo){
+    override suspend fun addCollection(collection: CollectionInfo) {
         val collectionEntity = CollectionsEntity(
             collectionId = 0,
             name = collection.name,
@@ -31,24 +32,69 @@ class RepositoryImpl @Inject constructor(
     }
 
     override suspend fun getCollectionWithProducts(collectionId: Int): List<ProductInfo> {
-        val products = database.collectionProductsCrossRefDao().getCollectionWithProducts(collectionId).products
+        val products = database.collectionProductsCrossRefDao()
+            .getCollectionWithProducts(collectionId).products
         val mapped = products.map { productEntity ->
             ProductInfo(
-            name = productEntity.name,
-            description = productEntity.description,
-            upcCode = productEntity.upcCode
-        ) }
+                name = productEntity.name,
+                description = productEntity.description,
+                upcCode = productEntity.upcCode,
+                id = productEntity.productId
+            )
+        }
         return mapped
     }
 
-    override suspend fun getProductsWithCollection(productId: Int): List<CollectionInfo> {
-        val collections = database.collectionProductsCrossRefDao().getProductsWithCollection(productId).collections
+    override suspend fun getCollections(): List<CollectionInfo> {
+        val collections = database.collectionsDao().getAllCollections()
         val mapped = collections.map { collectionEntity ->
             CollectionInfo(
                 collectionId = collectionEntity.collectionId,
                 name = collectionEntity.name,
                 description = collectionEntity.description
-            ) }
+            )
+        }
+        return mapped
+    }
+
+    override suspend fun getProductsWithCollection(productId: Int): List<CollectionInfo> {
+        val collections = database.collectionProductsCrossRefDao()
+            .getProductsWithCollection(productId).collections
+        val mapped = collections.map { collectionEntity ->
+            CollectionInfo(
+                collectionId = collectionEntity.collectionId,
+                name = collectionEntity.name,
+                description = collectionEntity.description
+            )
+        }
+        return mapped
+    }
+
+    // TODO convert to Flow
+    override suspend fun getCollectionsWithProducts(): List<CollectionWithProducts> {
+        val collections = database.collectionsDao().getAllCollections()
+        val mapped = collections.map { collectionEntity ->
+            val products = database
+                .collectionProductsCrossRefDao()
+                .getCollectionWithProducts(collectionEntity.collectionId)
+
+            // Map from entity to domain model
+            CollectionWithProducts(
+                collection = CollectionInfo(
+                    collectionId = collectionEntity.collectionId,
+                    name = collectionEntity.name,
+                    description = collectionEntity.description
+                ),
+                products = products.products.map { productEntity ->
+                    ProductInfo(
+                        name = productEntity.name,
+                        description = productEntity.description,
+                        upcCode = productEntity.upcCode,
+                        id = productEntity.productId
+                    )
+                }
+            )
+        }
         return mapped
     }
 
