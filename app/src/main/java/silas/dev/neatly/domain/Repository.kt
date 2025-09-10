e56835f5e06efaf68d94248ff5de9687add6ad1e
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2025-09-10 12:57:16 -0500

    Added method to get collections with products

--- app/src/main/java/silas/dev/neatly/domain/Repository.kt
+++ app/src/main/java/silas/dev/neatly/domain/Repository.kt
@@ -5,5 +5,12 @@ interface Repository {
     suspend fun addProduct(product: ProductInfo)
     suspend fun addCollection(collection: CollectionInfo)
     suspend fun getCollectionWithProducts(collectionId: Int): List<ProductInfo>
+    suspend fun getCollections(): List<CollectionInfo>
     suspend fun getProductsWithCollection(productId: Int): List<CollectionInfo>
+    suspend fun getCollectionsWithProducts(): List<CollectionWithProducts>
 }
+
+data class CollectionWithProducts(
+    val collection: CollectionInfo,
+    val products: List<ProductInfo>,
+)
