53583f353f58591c6eff152525fc7042791fd6e7
SilviaAsmat <35587004+SilviaAsmat@users.noreply.github.com>
2026-01-15 18:28:49 -0600

    Added app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "silas.dev.neatly"
         minSdk = 24
         targetSdk = 36
-        versionCode = 2
-        versionName = "2.0"
+        versionCode = 3
+        versionName = "2.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
